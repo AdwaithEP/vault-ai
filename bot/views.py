dc2988f0c040b9468ab1e0a93325069f6d9df501
AdwaithEP <adwaithpramod7@gmail.com>
2026-05-18 08:07:47 +0530

    switch to google-genai package

--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,8 @@
 import os
 import json
 from pyexpat import model
-import google.generativeai as genai
+from xmlrpc import client
+from google import genai
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
@@ -9,16 +10,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
 @login_required
 def chat(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         user_message = data.get('message', '')
-        model = genai.GenerativeModel('gemini-1.5-flash')
-        response = model.generate_content(
-    f"You are VaultAI, a password security assistant. Help the user with password strength, security advice, and password management tips. User says: {user_message}"
+        client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+        response = client.models.generate_content(
+        model='gemini-2.0-flash',
+        contents=f"You are VaultAI, a password security assistant. Help the user with password strength, security advice, and password management tips. User says: {user_message}"
 )
         return JsonResponse({'reply': response.text})
     return render(request, 'chat.html')
